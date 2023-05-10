57f1846662f5efc4e3da4c5d6f0c606422d48ff7
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2023-05-10 09:59:13 -0300

    Coleção vazia, o que retornar?

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.algamoney.api.resource;
 import com.algamoney.api.model.Categoria;
 import com.algamoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
