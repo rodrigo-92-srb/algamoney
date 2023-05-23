edf6cdb60266eea7d5944ddcf4302b060a8d241d
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2023-05-22 21:56:38 -0300

    identando código

--- src/main/java/com/algamoney/api/model/Endereco.java
+++ src/main/java/com/algamoney/api/model/Endereco.java
@@ -1,34 +1,18 @@
 package com.algamoney.api.model;
 
 import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @Embeddable
 public class Endereco {
 
-    @NotNull
-    @Size(min = 3, max = 30)
-    private String logradouro;
-    @NotNull
-    @Size(min = 1, max = 10)
-    private String numero;
-    @NotNull
-    @Size(min = 1, max = 10)
-    private String complemento;
-    @NotNull
-    @Size(min = 3, max = 30)
-    private String bairro;
-    @NotNull
-    @Size(min = 3, max = 30)
-    private String cep;
-    @NotNull
-    @Size(min = 3, max = 30)
-    private String cidade;
-    @NotNull
-    @Size(min = 3, max = 30)
-    private String estado;
 
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String estado;
 
     public String getLogradouro() {
         return logradouro;
